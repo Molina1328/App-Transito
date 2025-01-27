5d7c66d761c9127733509c1d89dc0652b170cf69
Sebas <elsebaslml@gmail.com>
2025-01-26 21:36:15 -0500

    Firebase

--- app-movil-transito/src/app/app.module.ts
+++ app-movil-transito/src/app/app.module.ts
@@ -6,11 +6,17 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({"projectId":"transitoapp-2025","appId":"1:259935818011:web:8b3e2ceb790a6b901103be","storageBucket":"transitoapp-2025.firebasestorage.app","apiKey":"AIzaSyCWVomBPghXmcBkPNmGhB3bg0D4m0yo9sE","authDomain":"transitoapp-2025.firebaseapp.com","messagingSenderId":"259935818011","measurementId":"G-VMQR67K5P9"})), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideStorage(() => getStorage())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
